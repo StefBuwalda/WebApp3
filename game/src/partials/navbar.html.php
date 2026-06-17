d4dd6e4673579f66dcc157263dd1611996310409
Anh-Thy04 <anhthytran2309@gmail.com>
2026-06-17 18:15:11 +0200

    added simple login page and changed (in progess) navbar

--- game/src/partials/navbar.html.php
+++ game/src/partials/navbar.html.php
@@ -1,7 +1,8 @@
 <nav id="navbar">
     <div class="nav-links">
-        <a href="#">Home</a>
+        <a href="#a">Home</a>
         <a href="#">About</a>
+        <img src="logo.png" alt="logo" id="logo">
         <a href="#">Contact</a>
         <a href="/login">Logout</a>
     </div>
