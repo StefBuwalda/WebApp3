1b565790359d765a9de78164a0d21032d305653c
Anh-Thy04 <anhthytran2309@gmail.com>
2026-06-20 19:36:24 +0200

    added modal for login and navbar change

--- game/src/pages/login.php
+++ game/src/pages/login.php
@@ -1,23 +1,35 @@
+<?php include __DIR__ . '/../partials/navbar.html.php';
+?>
+
 <div class="background">
     <div class="welcome_content">
         <h1>Hello and Welcome</h1>
         <h2> Had a rough day? Come play your favorite memory! </h2>
-        <div class="choice-area">
 
+        <div class="choice-area">
             <a href="" class="button" > Register </a>
-            <button class="button" onclick="login()" > Log in </button>
+            <input type="checkbox" id="modal-toggle" class="modal-checkbox">
+            <label for="modal-toggle" class="button-wrapper">
+                <a class="button" type="open-btn">Login</a>
+                </label>
+
+            <div class="modal-overlay" id="myModal">
+                <label for="modal-toggle" class="outside-close-area"></label>
+
+                <div class="modal-content">
+                    <label for="modal-toggle" class="close-btn">X</label>
+                    <label for="uname"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="uname" required>
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="psw" required>
+                    <button class="button" onclick="login()" > Log in </button>
+                </div>
+
+            </div>
+        </div>
             <br>
             <img src="cats.png" alt="kitties in a row">
-
-
         </div>
-        <!-- <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-  -->
-    </div>
 
 </div>
 
--- game/src/partials/navbar.html.php
+++ game/src/partials/navbar.html.php
@@ -1,8 +1,10 @@
 <nav id="navbar">
-    <div class="nav-links">
+    <div class="nav-left">
         <a href="#a">Home</a>
         <a href="#">About</a>
-        <img src="logo.png" alt="logo" id="logo">
+    </div>
+    <img src="logo.png" alt="logo" id="logo">
+    <div class="nav-right">
         <a href="#">Contact</a>
         <a href="/login">Logout</a>
     </div>
