86ebade4e3de503cd76a73d845191d52dc64b43e
Stef <stbuwalda@gmail.com>
2026-06-28 17:56:01 +0200

    Removed client hydration as SSR was removed

--- admin/src/app/app.config.ts
+++ admin/src/app/app.config.ts
@@ -1,16 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHighcharts } from 'highcharts-angular';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {routes} from './app.routes';
+import {provideHighcharts} from 'highcharts-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(),
     provideHighcharts(),
   ],
